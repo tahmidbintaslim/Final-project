b6d3515d43a4c9983ca3ae23d535348f1c29e7a7
Casper's PC <Casper's PC>
2016-06-10 15:13:36 +0800

    Final Project

---
+++ Run_study.R
@@ -0,0 +1,23 @@
+# Data Analysis and Visualization for Airbnb
+#
+# Big Data and Analytics for Business - INSEAD 2016
+#
+# Hederik de Vries (16D)
+# Lukas Cerno (16D)
+# Casper Roex (16D)
+#
+# In this case study we will use the Airbnb data
+# 
+
+# Clean the workspace
+rm(list=ls())
+
+#Set Final-project as Directory
+#setwd("..")
+
+# Set up the working directory
+local_directory <- getwd()
+
+# Install and load required packages
+source(paste(local_directory,"R/library.R", sep="/"))
+
